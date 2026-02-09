e5053427683a20f4a085999c0fe9233f1fea2f7b
Dearie <eburujacintha@gmail.com>
2026-02-09 22:31:41 +0100

    put the db in sync

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL,
+});
+
 // Middleware
 app.use(cors()); 
 app.use(express.json()); 
